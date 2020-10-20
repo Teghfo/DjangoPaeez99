d4ce4a135ffc0e74b7e6ba0cc9b0893d7a7654de
Teghfo <teghfo@gmail.com>
2020-10-20 18:40:24 +0330

    update lili!

--- W2/linked_list.py
+++ W2/linked_list.py
@@ -9,19 +9,44 @@ class LinkedList:
         self.root = None
         self.length = 0  # Optional
 
+    def __increment_length(self):
+        self.length += 1
+
     def add_begin(self, elm):
         if self.root == None:
             self.root = elm
         else:
             elm.next = self.root
             self.root = elm
-        self.length += 1
+        self.__increment_length()
 
     def add_end(self, elm):
-        pass
+        if self.root == None:
+            self.root = elm
+        else:
+            temp = self.root
+            while temp.next != None:
+                temp = temp.next
+            temp.next = elm
+        self.__increment_length()
 
     def add_between(self, elm0, newelm):
-        pass
+        temp = self.root
+        find_flag = False
+
+        while True:
+            if temp is elm0:
+                find_flag = True
+                break
+            elif temp.next == None:
+                break
+            temp = temp.next
+
+        if find_flag:
+            newelm.next = temp.next
+            temp.next = newelm
+        else:
+            raise "Not Found"
 
     def pop_elm(self):
         pass
@@ -45,9 +70,10 @@ class LinkedList:
 
 elm1 = Element("Khalil")
 elm2 = Element("Alireza")
+elm3 = Element("Niloufar")
 
 l = LinkedList()
-l.add_begin(elm1)
 l.add_begin(elm2)
-
+l.add_begin(elm1)
+l.add_between(elm1, elm3)
 print(l)
